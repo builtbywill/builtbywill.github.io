7dd9356f0ab53220d6526d8fa065b48c28c89c36
Will Grauvogel <builtbywill@gmail.com>
2014-03-16 15:04:13 -0400

    updated broken links on changelog

--- code/booklet/changelog.php
+++ code/booklet/changelog.php
@@ -52,7 +52,7 @@
         <p>This update brings major changes and a large overhaul to the booklet code.</p>
         <p>The booklet now runs almost entirely from CSS manipulation and does not directly modify the DOM as intensively, which should increase performance.</p>
         <p>The <strong>.b-load</strong> class wrapper is no longer needed in your markup. The plugin will ignore it if it does exist.</p>
-        <p>Using the <strong>$("#mybook").data("booklet")</strong> method, access to the Booklet() object is now open, giving access to some public methods listed below. See the <a href="options">options page</a> or <a href="examples/api-direct">direct api examples</a> for more information.</p>
+        <p>Using the <strong>$("#mybook").data("booklet")</strong> method, access to the Booklet() object is now open, giving access to some public methods listed below. See the <a href="documentation">options page</a> or <a href="demos/methods">direct api examples</a> for more information.</p>
 
         <ul>
             <li><strong>init()</strong></li>
@@ -77,37 +77,37 @@
         <p>A bug with IE7, IE8 has been fixed. Shadows will no longer appear as a black box in the reverse transitions.</p>
         <p>New options have been added as well for percentage sizing, auto play, and some tweaks for the closed book option.</p>
         <ul>
-            <li><a href="examples/closed">autoCenter</a></li>
-            <li><a href="examples/arrows">arrowsHide</a></li>
-            <li><a href="examples/auto">auto</a></li>
-            <li><a href="examples/auto">delay</a></li>
-            <li><a href="examples/auto">play</a></li>
-            <li><a href="examples/auto">pause</a></li>
+            <li><a href="demos/closed">autoCenter</a></li>
+            <li><a href="demos/arrows">arrowsHide</a></li>
+            <li><a href="demos/auto">auto</a></li>
+            <li><a href="demos/auto">delay</a></li>
+            <li><a href="demos/auto">play</a></li>
+            <li><a href="demos/auto">pause</a></li>
         </ul>
         
         <h3>1.1.0</h3>
         <h4>December 9, 2010</h4>
         <p>Due to some concerns set forth by the good people at <a href="http://viewbook.com" target="_blank">Viewbook.com</a> (name trademark, possible client confusion, etc.), my plugin is now called the <strong>jQuery Booklet Plugin</strong>.</p>
         <p>Along with the name change, I have re-worked the way the plugin works with the page data. It should now be compatiple with form elements (inputed data will not disappear on page turn now), CUFON generated text, and other content that needs separate initialization.</p>
-        <p>The <a href="examples/callbacks">before and after callbacks</a> can now access an optional "options" variable.</p>
+        <p>The <a href="demos/callbacks">before and after callbacks</a> can now access an optional "options" variable.</p>
         
         <h3>1.0.2</h3>
         <h4>December 1, 2010</h4>
         <p>New options have been added as well as streamlining certain parts of the code.</p>
         <ul>
-            <li><a href="examples/hovers">hovers</a></li>
-            <li><a href="examples/closed">closed</a></li>
-            <li><a href="examples/closed">covers</a></li>
-            <li><a href="examples/closed">closedFrontTitle</a></li>
-            <li><a href="examples/closed">closedFrontChapter</a></li>
-            <li><a href="examples/closed">closedBackTitle</a></li>
-            <li><a href="examples/closed">closedBackChapter</a></li>
-            <li><a href="examples/padding">pagePadding</a></li>
+            <li><a href="demos/hovers">hovers</a></li>
+            <li><a href="demos/closed">closed</a></li>
+            <li><a href="demos/closed">covers</a></li>
+            <li><a href="demos/closed">closedFrontTitle</a></li>
+            <li><a href="demos/closed">closedFrontChapter</a></li>
+            <li><a href="demos/closed">closedBackTitle</a></li>
+            <li><a href="demos/closed">closedBackChapter</a></li>
+            <li><a href="demos/padding">pagePadding</a></li>
         </ul>
         
         <h3>1.0.1</h3>
         <h4>November 29, 2010</h4>
-        <p>Added <a href="examples/direction">direction</a> option. Allows for <strong>RTL</strong> or <strong>LTR</strong> book flipping and organization.</p>
+        <p>Added <a href="demos/direction">direction</a> option. Allows for <strong>RTL</strong> or <strong>LTR</strong> book flipping and organization.</p>
         
         <h3>1.0.0</h3>
         <h4>November 23, 2010</h4>
