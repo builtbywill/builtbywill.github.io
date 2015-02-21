4a418e00fb3c3f7ab29a20d8aefa853b2e07b1a5
Will Grauvogel <builtbywill@gmail.com>
2015-02-21 13:50:20 -0500

    angular homepage updates, starting to add projects.

--- dev/angular/dev/app/home/home.js
+++ dev/angular/dev/app/home/home.js
@@ -9,7 +9,29 @@
     function HomeController($scope) {
 
         /*jshint validthis: true */
-        //var ctrl = this;
+        var ctrl = this;
+
+        ctrl.projects = [{
+            title: 'Pattern',
+            text: 'Pattern is a simple way to measure your study habits, providing you analytics and insights to become a better learner.',
+            url: 'http://www.itap.purdue.edu/studio/pattern/',
+            areas: [{
+                name: 'iOS',
+                text: 'Solo. Developed the iOS client for Pattern to be simple, clean and have life with transitions and animations.',
+                label: 'primary',
+                tech: ['Objective-C', 'CoreData', 'RestKit', 'POP', 'Cocoapods']
+            }, {
+                name: 'Web',
+                text: 'Team. We developed the Web Client using Angular JS. Learning a new technology is always fun.',
+                label: 'success',
+                tech: ['Angular JS', 'Grunt', 'Bootstrap']
+            }, {
+                name: 'Server',
+                text: 'Team. Developed to run on the cloud using Windows Azure, we made the server side lightweight and fast.',
+                label: 'info',
+                tech: ['C#', 'Windows Azure', 'Entity Framework', 'OData']
+            }]
+        }];
 
         activate();
 
