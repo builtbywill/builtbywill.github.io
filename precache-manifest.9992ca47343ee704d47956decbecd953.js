89a365806727400abcc14cf6405f2f4dcb3d5cff
Will Grauvogel <builtbywill@gmail.com>
2019-02-02 11:13:54 -0500

    Updates

---
+++ precache-manifest.9992ca47343ee704d47956decbecd953.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d4f13d7c9e3da2b16c9da68c98c11a9",
+    "url": "/static/media/hotseat-poll.0d4f13d7.png"
+  },
+  {
+    "revision": "785231076cd2b6487e9b",
+    "url": "/static/css/main.4688596f.chunk.css"
+  },
+  {
+    "revision": "44dfe8cc676882243911a3197a50169e",
+    "url": "/static/media/Lato-Bold.44dfe8cc.ttf"
+  },
+  {
+    "revision": "df072e9c1c4e0730a15b",
+    "url": "/static/js/1.df072e9c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8021374b239c1dbf5ce1e74076eb837b",
+    "url": "/static/media/pattern-cover.8021374b.png"
+  },
+  {
+    "revision": "cc469b3f57827134a57a1eb750d95d81",
+    "url": "/static/media/pattern-ios.cc469b3f.png"
+  },
+  {
+    "revision": "af31abe95d2c38a95232a390e4cc209d",
+    "url": "/static/media/pattern-apple-watch.af31abe9.png"
+  },
+  {
+    "revision": "04141ce9fd6679c13093f3a7a3351c99",
+    "url": "/static/media/pattern-learner.04141ce9.png"
+  },
+  {
+    "revision": "c8a018bb35a385448506c4ab5d857fa6",
+    "url": "/static/media/pattern-instructor-charts.c8a018bb.png"
+  },
+  {
+    "revision": "5f70355cb66400ebcbf4443bc8b01402",
+    "url": "/static/media/skyepack-cover.5f70355c.png"
+  },
+  {
+    "revision": "3c7d3db7d67d963176553fa999bc8f77",
+    "url": "/static/media/skyepack-ios.3c7d3db7.png"
+  },
+  {
+    "revision": "92cb30e0965839320be9c0295b7b0baf",
+    "url": "/static/media/skyepack-android.92cb30e0.png"
+  },
+  {
+    "revision": "1d4460a0402ee773a177a43fc0ea011f",
+    "url": "/static/media/skyepack-web-reader.1d4460a0.png"
+  },
+  {
+    "revision": "e1bbdfaf816127b0f118b748b7033f55",
+    "url": "/static/media/skyepack-authoring.e1bbdfaf.png"
+  },
+  {
+    "revision": "aa4d81fb6588c7993e4031e34a175edc",
+    "url": "/static/media/passport-cover.aa4d81fb.png"
+  },
+  {
+    "revision": "c4de394616017c3d0b67baa8f738605d",
+    "url": "/static/media/passport-iphone.c4de3946.png"
+  },
+  {
+    "revision": "ada35b078e05c247c22b25687d1722cd",
+    "url": "/static/media/passport-ipad-1.ada35b07.png"
+  },
+  {
+    "revision": "177cc24040413809c6a9e9e693d80ac7",
+    "url": "/static/media/passport-ipad-2.177cc240.png"
+  },
+  {
+    "revision": "905d15dbe17eb656a77c366c4d3948fc",
+    "url": "/static/media/passport-ipad-3.905d15db.png"
+  },
+  {
+    "revision": "054460e51d2ab2756e9f49d5db515fa3",
+    "url": "/static/media/passport-learner-dashboard.054460e5.png"
+  },
+  {
+    "revision": "591c4f44708d7af8852dff6e45dee1a4",
+    "url": "/static/media/passport-challenge.591c4f44.png"
+  },
+  {
+    "revision": "731631110fb4984ef9af71e61f02803d",
+    "url": "/static/media/passport-badge-creator.73163111.png"
+  },
+  {
+    "revision": "c5fd7fd901204a21f930a15226a20e30",
+    "url": "/static/media/hotseat-cover.c5fd7fd9.png"
+  },
+  {
+    "revision": "80c25f8e19bfe0a0e8d575e445a2239c",
+    "url": "/static/media/hotseat-ios.80c25f8e.png"
+  },
+  {
+    "revision": "cf4d874d17d83e504c6dc9950d1c795e",
+    "url": "/static/media/hotseat-feed.cf4d874d.png"
+  },
+  {
+    "revision": "785231076cd2b6487e9b",
+    "url": "/static/js/main.78523107.chunk.js"
+  },
+  {
+    "revision": "3ee470a78aeea3117261045140f6b4dc",
+    "url": "/static/media/hotseat-roster-sync.3ee470a7.png"
+  },
+  {
+    "revision": "02d6b94f241371e6c696c81a3c6993d8",
+    "url": "/static/media/convoy-cover.02d6b94f.png"
+  },
+  {
+    "revision": "d655d8effc1a8e97c05be3308757164f",
+    "url": "/static/media/convoy-ipad-slide.d655d8ef.png"
+  },
+  {
+    "revision": "65a2df64842a9dd8815ea493b343be15",
+    "url": "/static/media/convoy-ipad-child-slide.65a2df64.png"
+  },
+  {
+    "revision": "7604ffdacf3645f9e4e9abca1eb96afb",
+    "url": "/static/media/convoy-ipad-notes.7604ffda.png"
+  },
+  {
+    "revision": "94a09944c514197cb482b381f02be477",
+    "url": "/static/media/convoy-ipad-grid.94a09944.png"
+  },
+  {
+    "revision": "dc8fb583c29f23559a45d3cd632f2934",
+    "url": "/static/media/convoy-ipad-print.dc8fb583.png"
+  },
+  {
+    "revision": "a3e8afef1c8fc848ab2430a14e992f01",
+    "url": "/static/media/convoy-home.a3e8afef.png"
+  },
+  {
+    "revision": "3287dfca7718d5aeb0370c470d380379",
+    "url": "/static/media/convoy-edit-collection.3287dfca.png"
+  },
+  {
+    "revision": "5f08060aa7240505a54f30b2288e3728",
+    "url": "/static/media/booklet-cover.5f08060a.png"
+  },
+  {
+    "revision": "497c1c2d6209d6b8af1d1b6728908ce0",
+    "url": "/static/media/icomoon.497c1c2d.eot"
+  },
+  {
+    "revision": "f71d607d7bd1d95b2000762ed01ca12a",
+    "url": "/static/media/icomoon.f71d607d.ttf"
+  },
+  {
+    "revision": "8ce2db75cd95846e7f072da85187efe6",
+    "url": "/static/media/icomoon.8ce2db75.woff"
+  },
+  {
+    "revision": "a164e5673088bd14ca657a1eeba8a5d7",
+    "url": "/static/media/Ovo-Regular.a164e567.ttf"
+  },
+  {
+    "revision": "c8d87374ce8c85d5fdc365aa6bf4c06c",
+    "url": "/static/media/icomoon.c8d87374.svg"
+  },
+  {
+    "revision": "a567f4a8e101cb600004e09526a1a170",
+    "url": "/static/media/Lato-HairlineItalic.a567f4a8.ttf"
+  },
+  {
+    "revision": "3d747d8bf464744eecb91556d86de11b",
+    "url": "/static/media/Lato-LightItalic.3d747d8b.ttf"
+  },
+  {
+    "revision": "6c522f09efc8b83271666585f9fc6bf0",
+    "url": "/static/media/Lato-BlackItalic.6c522f09.ttf"
+  },
+  {
+    "revision": "77d353744697c77955f9bacc7f3ed90a",
+    "url": "/static/media/Lato-Black.77d35374.ttf"
+  },
+  {
+    "revision": "db15ac7981b2d2897d1a3c22892b5a51",
+    "url": "/static/media/Lato-Hairline.db15ac79.ttf"
+  },
+  {
+    "revision": "1ba4767ee37aab7e8d34fc339c3538cc",
+    "url": "/static/media/Lato-BoldItalic.1ba4767e.ttf"
+  },
+  {
+    "revision": "56c4cb26fd6a48b9c0ebcc07b376ee38",
+    "url": "/static/media/Lato-Italic.56c4cb26.ttf"
+  },
+  {
+    "revision": "5b761f2d1e4259ea6ac7ab3ebf7f3c49",
+    "url": "/static/media/Lato-Light.5b761f2d.ttf"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "/static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "df072e9c1c4e0730a15b",
+    "url": "/static/css/1.41ed5b05.chunk.css"
+  },
+  {
+    "revision": "53078fefa544ca50498277dd01a729cf",
+    "url": "/index.html"
+  }
+];
