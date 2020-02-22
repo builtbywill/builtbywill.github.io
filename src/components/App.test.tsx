84a4f31f925bdea1bc5a2fa7c65f863d66322fac
Will Grauvogel <builtbywill@gmail.com>
2020-02-22 15:00:36 -0500

    more dep updates, fix typeRoots

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-it('renders without crashing', () => {
-	const div = document.createElement('div')
-	ReactDOM.render(
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>,
-		div
-	)
-	ReactDOM.unmountComponentAtNode(div)
+describe('App', () => {
+	it('renders without crashing', () => {
+		const div = document.createElement('div')
+		ReactDOM.render(
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>,
+			div
+		)
+		ReactDOM.unmountComponentAtNode(div)
+	})
 })
