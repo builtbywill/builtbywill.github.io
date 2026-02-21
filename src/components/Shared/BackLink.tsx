32f2fcda49d569720ef07f8bed37caf856535d38
Will Grauvogel <code@builtbywill.com>
2026-02-21 11:47:24 -0500

    remove remaining react code, organize files

--- src/components/Shared/BackLink.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-export const BackLink = () => (
-	<Link to="/" className="absolute top-1 left-1 white no-underline bg-black-60 br3 f6 pa2 grow">
-		&laquo; Back
-	</Link>
-)
--- src/utils/window.ts
+++
@@ -1,4 +0,0 @@
-// istanbul ignore next: mocked while testing
-export function scrollToTop() {
-	window.scrollTo(0, 0)
-}
